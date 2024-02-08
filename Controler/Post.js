756f76f49b1b9725a6c9d3433b5b0f62e5bb0099
Mohmed932 <hindmahmoudfouad93@gmail.com>
2024-02-09 00:59:18 +0200

    posts controler

--- Controler/Post.js
+++ Controler/Post.js
@@ -1,6 +1,9 @@
 import { Post } from "../Models/Post.js";
-import fs from "fs"
+import fs from "fs";
+import { Deleteimage } from "../Utiles/Cloudinary.js";
 
+
+// create a new post
 export const CreatePost = async (req, res) => {
   try {
     if (!req.file) {
@@ -19,6 +22,49 @@ export const CreatePost = async (req, res) => {
     });
     fs.unlinkSync(`images/${req.file.filename}`);
     return res.json({ user });
+  } catch (error) {
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+// get all the posts
+export const GetPosts = async (req, res) => {
+  try {
+    const user = await Post.find().sort({ created_at: -1 });
+    return res.json({ user });
+  } catch (error) {
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+// get  single post
+export const SinglePost = async (req, res) => {
+  try {
+    const user = await Post.findOne({ _id: req.params.id });
+    return res.json({ user });
+  } catch (error) {
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+// delete a post
+export const DeletePost = async (req, res) => {
+  try {
+    const userPost = await Post.findOne({ _id: req.params.id });
+    if (!userPost) {
+      return res.status(400).json({
+        message: "there is some thing worng; posts id removed or not found ",
+      });
+    }
+    if (userPost.author.toString() === req.user._id || req.user.isAdmin) {
+      await Post.findByIdAndDelete(userPost._id);
+      Deleteimage(userPost.postImage.imageId);
+      return res.json({ message: "Post deleted successfully" });
+    } else {
+      return res.status(400).json({
+        message: "you are not allowed to remove this post",
+      });
+    }
   } catch (error) {
     return res.status(500).json({ error: "Internal Server Error" });
   }
--- Routing/Post.js
+++ Routing/Post.js
@@ -1,8 +1,10 @@
 import {Router} from 'express';
 import { verifyToken } from '../Utiles/VerifyToken.js';
 import { upload } from '../Utiles/Upload.js';
-import { CreatePost } from '../Controler/Post.js';
+import { CreatePost, DeletePost, GetPosts, SinglePost } from '../Controler/Post.js';
 
 export const PostRouter = Router();
 
 PostRouter.route("/createpost").post(verifyToken,upload.single("image"),CreatePost);
+PostRouter.route('/posts').get(GetPosts)
+PostRouter.route('/posts:id').get(SinglePost).delete(verifyToken,DeletePost)
