b2b8a9a725ea130a9ddfbc2ce76223925dc449c1
Mohmed932 <mohmedmahmoudfouad@gmail.com>
2024-03-19 21:48:26 +0200

    userinformation rote

--- Controler/UserInformation.js
+++ Controler/UserInformation.js
@@ -4,7 +4,7 @@ export const getUsers = async (req, res) => {
   try {
     const users = await User.find(
       {},
-      { _id: 1, username: 1, name: 1, surname: 1, follower: 1 }
+      { _id: 1, username: 1, name: 1, surname: 1, follower: 1, imageProfile: 1 }
     ).limit(10);
     return res.json({ users });
   } catch (error) {
