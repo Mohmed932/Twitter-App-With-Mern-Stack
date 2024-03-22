c1ebb89d23dbf928a0e0ec8f3aa2a3a10377faab
Hind <hindmahmoudfouad93@gmail.com>
2024-03-22 03:49:32 +0200

    cancel and get followers

--- Routing/UserInFormation.js
+++ Routing/UserInFormation.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { verifyToken } from "../Middelware/VerifyToken.js";
 import {
   AcceptFollowRequests,
+  CancelFollow,
   CancelFollowRequests,
   GetFolloweRequests,
   GetFollowers,
@@ -14,9 +15,9 @@ import { isValid } from "../Middelware/IsValid.js";
 export const UserInformation = Router();
 
 UserInformation.route("/users").get(verifyToken, getUsers);
-UserInformation.route("/users/Following/:id").get(verifyToken, GetFollowing);
-UserInformation.route("/users/followers/:id").get(verifyToken, GetFollowers);
-UserInformation.route("/users/GetFolloweRequests/:id").get(verifyToken, GetFolloweRequests);
+UserInformation.route("/users/Following/:id").get(isValid,verifyToken, GetFollowing);
+UserInformation.route("/users/followers/:id").get(isValid,verifyToken, GetFollowers);
+UserInformation.route("/users/GetFolloweRequests/:id").get(isValid,verifyToken, GetFolloweRequests);
 UserInformation.route("/users/sendfollowRequests/:id").put(
   isValid,
   verifyToken,
@@ -26,6 +27,11 @@ UserInformation.route("/users/AcceptFollowRequest/:id").put(
   isValid,
   verifyToken,
   AcceptFollowRequests
+);
+UserInformation.route("/users/CancelFollow/:id").put(
+  isValid,
+  verifyToken,
+  CancelFollow
 );
 UserInformation.route("/users/CancelFollowRequests/:id").put(
   isValid,
